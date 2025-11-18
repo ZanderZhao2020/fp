a3456435fec49d4e10f91830e4aca7bbd2b66b20
Zander Zhao <zanderscioly@gmail.com>
2025-11-18 18:43:46 +0000

    btd 5

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function play() {
 	let player = RufflePlayer.newest().createPlayer();
 	document.body.appendChild(player);
-	player.ruffle().load(document.getElementById("selector").value + ".swf");
+	let fileInp = document.getElementById("file");
+	player.ruffle().load(fileInp.files.length === 0 ? document.getElementById("selector").value + ".swf" : URL.createObjectURL(fileInp.files[0]));
 	document.getElementById("options").remove();
 }
