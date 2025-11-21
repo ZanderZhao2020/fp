8f145a73c554ddaa67a5df989a0ac685406b6039
Zander Zhao <zanderscioly@gmail.com>
2025-11-21 21:38:42 +0000

    burgeria

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 "use strict";
+let selector = document.getElementById("selector");
 function play() {
 	let player = RufflePlayer.newest().createPlayer();
 	document.body.appendChild(player);
 	let fileInp = document.getElementById("file");
-	player.ruffle().load(fileInp.files.length === 0 ? document.getElementById("selector").value + ".swf" : URL.createObjectURL(fileInp.files[0]));
+	if (fileInp.files.length === 0) {
+		localStorage.lastGame = selector.value;
+		player.ruffle().load(selector.value + ".swf");
+	} else {
+		player.ruffle().load(URL.createObjectURL(fileInp.files[0]));
+	}
 	document.getElementById("options").remove();
+}
+if (localStorage.lastGame !== undefined) {
+	selector.value = localStorage.lastGame;
 }
